69e988d949c6c4beac2402df6ba27aa53b888dd0
Ankush Singh Bhadauriya <2k23.it1a.2310162@gmail.com>
2025-01-15 00:06:25 +0530

    Update 2.0 script.js

--- script.js
+++ script.js
@@ -17,17 +17,30 @@ SRValue = document.getElementById("SRValue"),
 SSValue = document.getElementById("SSValue"),
 CValue = document.getElementById("CValue"),
 UVValue = document.getElementById("UVValue"),
-PValue = document.getElementById("PValue");
+PValue = document.getElementById("PValue"),
+Forecast = document.querySelector(".Forecast");
 
-WEATHER_API_ENDPOINT="http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=cefcd70f32405f5998871bb3dff62dba&q=";
-WEATHER_DATA_ENDPOINT=''
-
-Forecast = DocumentTimeline.querySelector(".Forecast");
+WEATHER_API_ENDPOINT="https://api.openweathermap.org/data/3.0/weather?appid=cefcd70f32405f5998871bb3dff62dba&q=";
+WEATHER_DATA_ENDPOINT="https://api.openweathermap.org/data/3.0/onecall?appid=cefcd70f32405f5998871bb3dff62dba&exclude=minutely&units=metric&";
 // Function Declaration
-function findUserLocation() {
-  fetch(WEATHER_API_ENDPOINT + "London")
-  .then((response) => response.json())
-  .then((data) => {
+function finduserlocation() {
+  fetch(WEATHER_API_ENDPOINT + userlocation.value)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.cod!='' && data.cod!=200){
+        alert(data.message);
+      return;
+    }
+    console.log(data);
+
+    city.innerHTML=data.name;
+
+    fetch(
+      WEATHER_DATA_ENDPOINT + 'lon=${data.coord.lon}&lat=${data.coord.lat}'
+    )
+    .then((response) => response.json())
+    then((data) => {
     console.log(data);
   });
+});
 }
